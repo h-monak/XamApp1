fd05d691a05ae57e21ceeb2bcff18772ccd8f21c
h-monak <secretandwhisper@hotmail.co.jp>
2018-05-27 21:24:17 +0900

    プロジェクト ファイルを追加します。

---
+++ XamApp1/XamApp1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamApp1.Models;
+
+namespace XamApp1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
